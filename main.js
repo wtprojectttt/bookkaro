1cc046f932c66cf36f4e953c7a81d8c284b92846
Manuqlly <selvaperumal20005@gmail.com>
2023-10-23 00:11:48 +0530

    Ban kai ikiyo kyoka suigetsu

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 document.getElementById('mlogo').addEventListener('click', () =>{
     window.location.href = ('main.html');
 })
+
+document.getElementById('planeB').addEventListener('click', () =>{
+    window.location.href = ('fbooking.html');
+})
+
+document.getElementById('busB').addEventListener('click', () =>{
+    window.location.href = ('bbooking.html');
+})
+
+document.getElementById('trainB').addEventListener('click', () =>{
+    window.location.href = ('tbooking.html');
+})
+
+document.getElementById('carB').addEventListener('click', () =>{
+    window.location.href = ('cbooking.html');
+})
+
