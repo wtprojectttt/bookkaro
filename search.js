10a407bfdb67047ec56e8c960e663b4e6c35527e
Rageemon <145965461+Rageemon@users.noreply.github.com>
2024-03-30 16:54:24 +0530

    Update search.js

--- search.js
+++ search.js
@@ -5,4 +5,20 @@ $(document).ready(function () {
             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         });
     });
+
+    let table;
+
+    table=$("table").DataTable({
+        pageLength : 13,
+        colReader: true,
+        columns : columns,
+        data: data
+
+
+    });
+
+    $("table").on("click","tbody tr",function (){
+        const ro = table.row().data();
+        console.log(ro);
+    });
 });
